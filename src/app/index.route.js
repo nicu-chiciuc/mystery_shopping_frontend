68fa697b900d66d0ec6f334833b99bbddeb8dff0
iulian <iulian.gulea@gmail.com>
2016-01-11 18:30:05 +0200

    Configured client and authentication states.

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -13,7 +13,64 @@
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
-      });
+      })
+
+      .state('authenticated', {
+        templateUrl: 'app/authentication/authenticated.html',
+        abstract: true,
+        resolve: {
+          user: function (AuthToken, $state, $q) {
+          return AuthToken.getAuthenticatedAccount().then(
+            function ( account ) { return account.user; },
+            // must return a rejected promise in order to stay in rejected-mode
+            function () { return $q.reject( $state.go('login') ); });
+          },
+          onEnter: function (user) {
+            user.initialize();
+          }
+          //onExit: function (user) {
+          //  user.close();
+          //}
+        }
+      })
+      .state('authentication.login', {
+        url: '/login',
+        templateUrl: 'app/authentication/login/login.html',
+        controller: 'LoginController',
+        controllerAs: 'vm'
+      })
+      .state('clients', {
+        parent: 'authenticated',
+        url: '/clients',
+        templateUrl: 'app/clients/clients.html',
+        controller: 'ClientController',
+        controllerAs: 'vm',
+        resolve: {
+          clients: function ( user, models ) { return models.clients().getList(); }
+        }
+      })
+      .state('clients.new', {
+        parent: 'authenticated',
+        url: '/new',
+        templateUrl: 'app/clients/new/client-form.html',
+        controller: 'ClientFormController',
+        controllerAs: 'vm'
+      })
+      .state('clients.edit', {
+        parent: 'authenticated',
+        url: '/{id:int}/edit',
+        templateUrl: 'app/clients/edit/client-form.html',
+        controller: 'ClientFormController',
+        controllerAs: 'vm'
+      })
+      .state('clients.detail', {
+        parent: 'authenticated',
+        url: '/{id:int}',
+        templateUrl: 'app/clients/detail/client-detail.html',
+        controller: 'ClientController',
+        controllerAs: 'vm'
+      })
+    ;
 
     $urlRouterProvider.otherwise('/');
   }
