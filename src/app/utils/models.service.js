0ad75a0029d3c1e4d017c5fb33305f6de6df2b74
iulian <iulian.gulea@gmail.com>
2016-01-27 12:29:58 +0200

    Adjusted questionnaire templates endpoint.

--- src/app/utils/models.service.js
+++ src/app/utils/models.service.js
@@ -61,7 +61,7 @@
     }
 
     function questionnaireTemplates () {
-      return Restangular.service('questionnairetemplates');
+      return Restangular.service('templatequestionnaires');
     }
 
     function scripts () {
