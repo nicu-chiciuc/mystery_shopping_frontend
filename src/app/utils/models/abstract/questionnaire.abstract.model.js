ed9758ae3a5b5bb2150088ccd5a80e9169bba953
iulian gulea <iulian.gulea@gmail.com>
2016-02-03 20:30:47 +0200

    Created questionnaire abstract model and inherited from it in [template]questionnaire models.

--- src/app/utils/models/abstract/questionnaire.abstract.model.js
+++ src/app/utils/models/abstract/questionnaire.abstract.model.js
@@ -7,23 +7,85 @@
     .factory('AbstractQuestionnaireModel', AbstractQuestionnaireModel);
 
   /** @ngInject */
-  function AbstractQuestionnaireModel () {
+  function AbstractQuestionnaireModel ( TemplateQuestionnaireBlockModel ) {
     var Model = {
-      initializeAbstract: initializeAbstract,
-      convertFlatToNestedStructure: convertFlatToNestedStructure
+      initializeAbstractQuestionnaire: initializeAbstractQuestionnaire,
+      convertFlatToNestedStructure: convertFlatToNestedStructure,
+      postProcess: postProcess
     };
 
     return Model;
 
 
-    function initializeAbstract () {
+    function initializeAbstractQuestionnaire () {
       var questionnaire = this;
 
+      // Set weight of questionnaire to 100 so that child blocks can compute
+      // available weight based on this weight.
+      questionnaire.weight = 100;
+
       questionnaire.convertFlatToNestedStructure();
+
+      _.forEach(questionnaire[questionnaire.childBlocksProp], function (block) {
+        angular.extend(block, TemplateQuestionnaireBlockModel);
+        block.initialize();
+      });
     }
 
     function convertFlatToNestedStructure () {
       var questionnaire = this;
+    }
+
+    function postProcess () {
+      var questionnaire = this;
+      var processedQuestionnaire = _.cloneDeep(questionnaire);
+
+      processedQuestionnaire[questionnaire.childBlocksProp] = flattenQuestionnaireBlocks(questionnaire, questionnaire[questionnaire.childBlocksProp]);
+      return processedQuestionnaire;
+    }
+
+    function flattenQuestionnaireBlocks ( questionnaire, blocks ) {
+      var flattenedBlocks = [];
+
+      _.forEach(blocks, function ( block ) {
+        flattenQuestionnaireBlock(questionnaire, null, block, 0, flattenedBlocks);
+      });
+
+      return flattenedBlocks;
+    }
+
+    function flattenQuestionnaireBlock ( questionnaire, parentBlock, block, level, flattenedBlocksList ) {
+      block.level = level;
+
+      // If parent block doesn't exist, this means this is a top level block,
+      // thus set lft = 1.
+      // If parent block doesn't have latestPosition key, this means that it's
+      // the first child block of parent bloc, so set lft to parents next value.
+      // If parent block has latestPosition, this means that the latest child block
+      // has this position on the rght, which means that the lft value should be ++'d.
+      block.lft = parentBlock
+        ? (parentBlock.latestPosition
+        ? parentBlock.latestPosition + 1
+        : parentBlock.lft + 1)
+        : 1;
+
+      // Dive recursively into child blocks.
+      _.forEach(block[questionnaire.childBlocksProp], function ( childBlock ) {
+        flattenQuestionnaireBlock(block, childBlock, level + 1, flattenedBlocksList);
+      });
+
+      // After dealing with child blocks, set rght key.
+      // In case there is no latestPosition key, this means the block is a leaft, thus set lft++ as rght value.
+      // In case there is latestPosition key, set the next value as rght value.
+      block.rght = block.latestPosition ? block.latestPosition + 1 : block.lft + 1;
+
+      // Update parentBlock with latest position of the current block after setting
+      // the rght value so it can use it for next blocks in the tree.
+      if ( parentBlock ) parentBlock.latestPosition = block.rght;
+
+      flattenedBlocksList.push(block);
+
+      delete block[questionnaire.childBlocksProp];
     }
 
   }
--- src/app/utils/models/templateQuestionnaire.model.js
+++ src/app/utils/models/templateQuestionnaire.model.js
@@ -7,10 +7,9 @@
     .factory('TemplateQuestionnaireModel', TemplateQuestionnaireModel);
 
   /** @ngInject */
-  function TemplateQuestionnaireModel ( TemplateQuestionnaireBlockModel, AbstractParentBlockModel ) {
+  function TemplateQuestionnaireModel ( AbstractQuestionnaireModel, AbstractParentBlockModel ) {
     var Model = {
-      initialize: initialize,
-      postProcess: postProcess
+      initialize: initialize
     };
 
     return Model;
@@ -19,10 +18,6 @@
     function initialize () {
       var questionnaire = this;
 
-      // Set weight of questionnaire to 100 so that child blocks can compute
-      // available weight based on this weight.
-      questionnaire.weight = 100;
-
       // Child blocks property name
       questionnaire.childBlocksProp = 'template_blocks';
 
@@ -30,67 +25,13 @@
       questionnaire.childQuestionsProp = 'template_questions';
 
       // Question choices property name
-      questionnaire.questionChoicesProp = 'template_question_choices';
+      questionnaire.questionChoicesProp = 'template_question_choices_repr';
+
+      angular.extend(questionnaire, AbstractQuestionnaireModel);
+      questionnaire.initializeAbstractQuestionnaire();
 
       angular.extend(questionnaire, AbstractParentBlockModel);
       questionnaire.initializeAbstract(questionnaire.childBlocksProp);
-
-      _.forEach(questionnaire.template_blocks, function (block) {
-        angular.extend(block, TemplateQuestionnaireBlockModel);
-        block.initialize();
-      });
-    }
-
-    function postProcess () {
-      var questionnaire = this;
-      var processedQuestionnaire = _.cloneDeep(questionnaire);
-
-      processedQuestionnaire.template_blocks = flattenQuestionnaireBlocks(questionnaire.template_blocks);
-      return processedQuestionnaire;
-    }
-
-    function flattenQuestionnaireBlocks ( blocks ) {
-      var flattenedBlocks = [];
-
-      _.forEach(blocks, function ( block ) {
-        flattenQuestionnaireBlock(null, block, 0, flattenedBlocks);
-      });
-
-      return flattenedBlocks;
-    }
-
-    function flattenQuestionnaireBlock ( parentBlock, block, level, flattenedBlocksList ) {
-      block.level = level;
-
-      // If parent block doesn't exist, this means this is a top level block,
-      // thus set lft = 1.
-      // If parent block doesn't have latestPosition key, this means that it's
-      // the first child block of parent bloc, so set lft to parents next value.
-      // If parent block has latestPosition, this means that the latest child block
-      // has this position on the rght, which means that the lft value should be ++'d.
-      block.lft = parentBlock
-        ? (parentBlock.latestPosition
-          ? parentBlock.latestPosition + 1
-          : parentBlock.lft + 1)
-        : 1;
-
-      // Dive recursively into child blocks.
-      _.forEach(block.template_blocks, function ( childBlock ) {
-        flattenQuestionnaireBlock(block, childBlock, level + 1, flattenedBlocksList);
-      });
-
-      // After dealing with child blocks, set rght key.
-      // In case there is no latestPosition key, this means the block is a leaft, thus set lft++ as rght value.
-      // In case there is latestPosition key, set the next value as rght value.
-      block.rght = block.latestPosition ? block.latestPosition + 1 : block.lft + 1;
-
-      // Update parentBlock with latest position of the current block after setting
-      // the rght value so it can use it for next blocks in the tree.
-      if ( parentBlock ) parentBlock.latestPosition = block.rght;
-
-      flattenedBlocksList.push(block);
-
-      delete block.template_blocks;
     }
   }
 })();
